723aedd3c69db630f397e84a53ecbe3dd0d7c029
hfhoffman1144 <hfh160130@utdallas.edu>
2022-10-05 09:55:04 -0500

    kafka working

---
+++ consumer/main.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+from json import loads
+
+TOPIC_NAME = "phone-stream"
+KAFKA_SERVER = "localhost:9092"
+
+consumer = KafkaConsumer(
+     TOPIC_NAME,
+     bootstrap_servers=[KAFKA_SERVER],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    message = message.value
+    print(message)
---
+++ producer/main.py
@@ -0,0 +1,36 @@
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel, StrictStr
+from typing import List, Dict
+from kafka import KafkaConsumer, KafkaProducer
+
+TOPIC_NAME = "phone-stream"
+KAFKA_SERVER = "localhost:9092"
+
+producer = KafkaProducer(
+    bootstrap_servers = KAFKA_SERVER,
+    api_version = (0, 11, 15)
+)
+
+class SensorReading(BaseModel):
+    messageId: int
+    sessionId: str
+    deviceId: str
+    payload: List[Dict]
+
+app = FastAPI()
+
+
+@app.post("/phone-producer/")
+async def kafka_produce(data: SensorReading):
+
+    json_payload = json.dumps(data.dict())
+    json_payload = str.encode(json_payload)
+    producer.send(TOPIC_NAME, json_payload)
+    producer.flush()
+    
+    return 200
+
+
+
+
